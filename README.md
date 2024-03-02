a07a0e186e4c37ba2c5b3911cc0af567a21f2324
herbertribeiro19 <hebert11223@gmail.com>
2024-03-02 18:11:25 -0300

    lasts updates

---
+++ README.md
@@ -0,0 +1,8 @@
+# Portfólio profissional
+
+> Portfólio Profissional
+
+[Clique aqui para acessar](https://herbertribeiro19.github.io/Portifolio/)
+
+## Contato
+hebert11223@gmail.com
--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -130,7 +130,7 @@ const Contact = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    emailjs.sendForm('service_tox7kqs', 'template_nv7k7mj', form.current, 'SybVGsYS52j2TfLbi')
+    emailjs.sendForm('service_ewpn07s', 'template_spdm7js', form.current, '3uSuht1cjgcRGCwIT')
       .then((result) => {
         setOpen(true);
         form.current.reset();
